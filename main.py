2fcdf57cabb5ad3079683d754b9901ecb5033002
Surya Prakash <surya96t@gmail.com>
2024-04-15 17:25:17 -0400

    added basic flask structure

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect, request, url_for, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("base.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
