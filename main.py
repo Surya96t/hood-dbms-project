5f12921e24268d5df4648cbabb547d2d2cf51fdf
matthewcurran <matthewcurran@users.noreply.github.com>
2024-04-24 12:55:21 -0400

    Added @app.route('/add_res'...
    Signed-off-by: matthewcurran <matthewcurran@users.noreply.github.com>

--- main.py
+++ main.py
@@ -16,8 +16,15 @@ def check_res():
     return render_template("check_reservation.html")
 
 @app.route('/order_food')
-def order_fod():
+def order_food():
     return render_template("order_food.html")
+
+@app.route('/add_res', methods=['POST'])
+def add_res():
+    create(request.form['cust_name'], request.form['cust_email'], request.form['cust_num'],
+           request.form['cust_date'], request.form['cust_time'], request.form['cust_size'],
+           request.form['cust-requests'])
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
